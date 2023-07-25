9f9fa53142cd1ef85b4cf423c6edb0f3405050f2
NitinRaj0518 <129197313+NitinRaj0518@users.noreply.github.com>
2023-07-25 12:22:09 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 exports.dbConn = ()=>{
-    const dbURL ="mongodb+srv://nitinraj:mongonitin123@cluster0.thqvugf.mongodb.net/testdb?retryWrites=true&w=majority"
+    const dbURL ="mongodb+srv://name:password@cluster0.thqvugf.mongodb.net/testdb?retryWrites=true&w=majority"
     mongoose.connect(dbURL,{useNewUrlParser:true,useUnifiedTopology:true}).then((result)=>{
     console.log("Connected to database");
     }).catch((err)=>console.error(err));
